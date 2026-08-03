a9b5fb8072e37d5f3aec0d77f927e1bbe10d088f
kartikaygupta005 <kartikeygupta005@gmail.com>
2026-08-03 22:31:01 +0530

    dom1

---
+++ logic.js
@@ -0,0 +1,9 @@
+/*console.dir(document.body);*/
+let heading = document.getElementById("heading");
+console.log(heading);
+ let classHeading = document.getElementsByClassName("heading");
+ console.dir(classHeading);
+ console.log("classHeading");
+
+ let element  = document.querySelector("p")
+  //dom manipulation
