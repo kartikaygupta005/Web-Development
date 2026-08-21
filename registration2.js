36f3c70c2abe5bb270b64c68c16ac0352bc46afe
kartikaygupta005 <kartikeygupta005@gmail.com>
2026-08-21 18:07:22 +0530

    Add registration and reset event functions

---
+++ registration2.js
@@ -0,0 +1,25 @@
+function eventRegister(ele){
+    var name = document.getElementById("StudentNameInput");
+    var EmailAddress = document.getElementById("EmailAdressInput");
+    var MobileNumber = document.getElementById("MobileNumberInput");
+    var DOB = document.getElementById("DOBInput");
+    var Gender = document.getElementById("rad");
+    var Course = document.getElementById("Selectid");
+    var Adress = document.getElementById("AdressInput");
+    var Password = document.getElementById("PasswordInput");
+    
+    // localStorage.setItem()
+
+}
+function eventReset(ele){
+    var name = document.getElementById("StudentNameInput");
+    var EmailAddress = document.getElementById("EmailAdressInput");
+    var MobileNumber = document.getElementById("MobileNumberInput");
+    var DOB = document.getElementById("DOBInput");
+    var Gender = document.getElementById("rad");
+    var Course = document.getElementById("Selectid");
+    var Adress = document.getElementById("AdressInput");
+    var Password = document.getElementById("PasswordInput");
+
+
+}
